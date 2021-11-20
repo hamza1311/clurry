ce3494c1f927449cb272f3c8a0b1e4ef6c0558c0
Hamza <muhammadhamza1311@gmail.com>
2021-11-20 22:41:05 +0500

    no lazy

--- src/components/profile/ChangePassword.tsx
+++ src/components/profile/ChangePassword.tsx
@@ -1,18 +1,11 @@
 import { makeStyles, createStyles } from '@mui/styles'
-import React, { lazy, Suspense, useState } from 'react'
-import { Button } from '@mui/material'
+import React, { useState } from 'react'
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material'
 import PasswordField from '../utils/PasswordField'
 import { updatePassword, EmailAuthProvider, reauthenticateWithCredential } from 'firebase/auth'
 import useUser from '../../utils/hooks/useUser'
-import LoadingIndicator from '../utils/LoadingIndicator'
 import { Theme } from '@mui/material'
-
-const Dialog = lazy(() => import('@mui/material/Dialog'))
-const DialogActions = lazy(() => import('@mui/material/DialogActions'))
-const DialogContent = lazy(() => import('@mui/material/DialogContent'))
-const DialogTitle = lazy(() => import('@mui/material/DialogTitle'))
-
-const Snackbar = lazy(() => import('../utils/Snackbar'))
+import Snackbar from '../utils/Snackbar'
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
     root: {
@@ -74,49 +67,46 @@ export default function ChangePassword({ dialogOpen, setDialogOpen }: { dialogOp
     }
 
     return (<>
-        <Suspense fallback={<LoadingIndicator isVisible={true} />}>
-            <section className={classes.root}>
+        <section className={classes.root}>
+            <Dialog open={dialogOpen} onClose={handleClose} aria-labelledby='change-password-dialog-title'>
+                <DialogTitle id='change-password-dialog-title'>Change Password</DialogTitle>
 
-                <Dialog open={dialogOpen} onClose={handleClose} aria-labelledby='change-password-dialog-title'>
-                    <DialogTitle id='change-password-dialog-title'>Change Password</DialogTitle>
+                <DialogContent className={classes.dialogContent}>
+                    <PasswordField
+                        disabled={changingPassword}
+                        value={oldPassword}
+                        label='Old password'
+                        onChange={(e) => setOldPassword(e.target.value)}
+                    />
 
-                    <DialogContent className={classes.dialogContent}>
-                        <PasswordField
-                            disabled={changingPassword}
-                            value={oldPassword}
-                            label='Old password'
-                            onChange={(e) => setOldPassword(e.target.value)}
-                        />
+                    <PasswordField
+                        disabled={changingPassword}
+                        value={newPassword}
+                        label='New password'
+                        onChange={(e) => setNewPassword(e.target.value)}
+                    />
 
-                        <PasswordField
-                            disabled={changingPassword}
-                            value={newPassword}
-                            label='New password'
-                            onChange={(e) => setNewPassword(e.target.value)}
-                        />
+                    <PasswordField
+                        disabled={changingPassword}
+                        value={confirmNewPassword}
+                        label='Confirm new password'
+                        onChange={(e) => setConfirmNewPassword(e.target.value)}
+                    />
 
-                        <PasswordField
-                            disabled={changingPassword}
-                            value={confirmNewPassword}
-                            label='Confirm new password'
-                            onChange={(e) => setConfirmNewPassword(e.target.value)}
-                        />
+                    <Button
+                        className={classes.changePasswordButton}
+                        disabled={changingPassword}
+                    >Reset password</Button>
 
-                        <Button
-                            className={classes.changePasswordButton}
-                            disabled={changingPassword}
-                        >Reset password</Button>
+                    {error && error}
+                </DialogContent>
 
-                        {error && error}
-                    </DialogContent>
-
-                    <DialogActions>
-                        <Button onClick={handleClose} disabled={changingPassword}>Cancel</Button>
-                        <Button onClick={changePassword} disabled={changingPassword}>Update</Button>
-                    </DialogActions>
-                </Dialog>
-            </section>
-            <Snackbar message='Password changed successfully' open={snackbarOpen} setOpen={setSnackbarOpen}/>
-        </Suspense>
+                <DialogActions>
+                    <Button onClick={handleClose} disabled={changingPassword}>Cancel</Button>
+                    <Button onClick={changePassword} disabled={changingPassword}>Update</Button>
+                </DialogActions>
+            </Dialog>
+        </section>
+        <Snackbar message='Password changed successfully' open={snackbarOpen} setOpen={setSnackbarOpen}/>
     </>)
 }
